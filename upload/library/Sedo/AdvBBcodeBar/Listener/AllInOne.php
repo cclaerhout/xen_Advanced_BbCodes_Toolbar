feae6d35e62e526be99913d06e7f9dfd09274aa8
cclaerhout <cedric.claerhout@free.fr>
2014-01-29 01:09:11 +0800

    Version 3.5.1 released - preview
    A few improvements
    Compatible with TinyMCE Quattro 2.1.0

--- upload/library/Sedo/AdvBBcodeBar/Listener/AllInOne.php
+++ upload/library/Sedo/AdvBBcodeBar/Listener/AllInOne.php
@@ -15,9 +15,15 @@ class Sedo_AdvBBcodeBar_Listener_AllInOne
 	/***
 	 *  MCE INTEGRATION
 	 **/	
-	public static function MceIntegration(array &$plugins, array &$mceOptions, array &$mceBtnCss, array $extraValues)
+	public static function MceIntegration($mceConfigObj)
 	{
-		if(in_array('tags_highlighter', $extraValues['availableButtons']))
+		if(is_array($mceConfigObj))
+		{
+			return false;
+		}
+		$xenOptions = XenForo_Application::get('options');
+		
+		if($mceConfigObj->buttonIsEnabled('tags_highlighter'))
 		{
 			$hlParams = array(
 				'adv_hl_norm' => array(
@@ -34,9 +40,24 @@ class Sedo_AdvBBcodeBar_Listener_AllInOne
 				'adv_hl_tag_separator' => XenForo_Template_Helper_Core::styleProperty('adv_highlight_tag_options_separator')			
 			);
 			
-			$plugins[] = 'xenadvhl';
-			$mceOptions['params'] += $hlParams;
+			$mceConfigObj->addMcePlugin('xenadvhl');
+			$mceConfigObj->addBulkMceParams($hlParams);
 		}
+
+		if($xenOptions->sedo_bbm_adv_tinyquattro_menu_integration)
+		{
+			$menuItems = array(
+				'bbm_bimg', 'bbm_at_bbm_slides', '@adv_insert_1', '|',
+				 'bbm_article', 'bbm_fieldset', 'bbm_encadre', 'bbm_spoilerbb', '@adv_insert_2', '|', 
+				 'bbm_latex', 'bbm_gview', 'bbm_picasa', '@adv_insert_3');
+				 
+			$mceConfigObj->addMenu('adv_insert', 'insert',  'Advanced Insert', $menuItems);
+			
+			$mceConfigObj->addMenuItem('tags_highlighter', 'view', '@view_1', true);
+		}
+		
+		//Zend_Debug::dump($mceConfigObj->getMenusGrid());
+		//Zend_Debug::dump($mceConfigObj->getAvailableButtons());
 	}
 }
 //Zend_Debug::dump($abc);
